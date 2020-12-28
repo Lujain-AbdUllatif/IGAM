8c6161cc84df8f1e05ca193b083cfd0969d7ba32
Ammar Yusef <amar.yousef@gmail.com>
2020-12-28 20:55:14 +0200

    small fix within Table component...

--- src/igam/components/Table/index.js
+++ src/igam/components/Table/index.js
@@ -5,14 +5,14 @@ export default function Table(props) {
   return (
     <div className="packges-details">
       <div className="packges-details-head">
-        {props.table_titles.map((tilte) => {
+        {props.table_titles.map((title) => {
           return (
             <h5
               className={
                 props.className ? props.className : "packge-detail-title"
               }
             >
-              title
+              {title}
             </h5>
           );
         })}
